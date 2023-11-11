f20c415976c733efa5a00ae7b142ec738cd90cf5
Định Phạm Xuân <phamdinh5302@gmail.com>
2023-11-11 22:58:47 +0700

    add quiz

--- app/src/main/java/com/example/gamepuzzlepursuit/HomeActivity.java
+++ app/src/main/java/com/example/gamepuzzlepursuit/HomeActivity.java
@@ -15,7 +15,11 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -26,6 +30,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -43,6 +53,8 @@ public class HomeActivity extends AppCompatActivity implements
     //Khai báo đối tượng Google Map
     private GoogleMap mMap;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,8 +65,67 @@ public class HomeActivity extends AppCompatActivity implements
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //nut cau hoi
+        Button btnquiz = findViewById(R.id.btnquiz);
+        btnquiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizDialog(Gravity.CENTER);
+
+            }
+        });
+
+
+    }
+    //show cau hoi
+    private void openQuizDialog(int gravity){
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_dialog_quiz);
+
+        Window window = dialog.getWindow();
+        if (window == null){
+            return;
+        }
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = gravity;
+        window.setAttributes(windowAttributes);
+
+        if (Gravity.CENTER == gravity){
+            dialog.setCancelable(true);
+        }else {
+            dialog.setCancelable(false);
+        }
+//        //cau hoi//
+//        TextView questionTextView;
+//        Button ansA, ansB, ansC, ansD;
+//        Button submitBtn;
+//        int currentQuestionIndex = 0;
+//        //end//
+//        questionTextView = findViewById(R.id.question);
+//        ansA = findViewById(R.id.ans_A);
+//        ansB = findViewById(R.id.ans_B);
+//        ansC = findViewById(R.id.ans_C);
+//        ansD = findViewById(R.id.ans_D);
+//        submitBtn = findViewById(R.id.submit_btn);
+//
+//
+//        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
+//        ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+//        ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+//        ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+//        ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
+
+        dialog.show();
     }
 
+
+
+    //Begin location
     @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
@@ -67,7 +138,6 @@ public class HomeActivity extends AppCompatActivity implements
         enableMyLocation();
         mMap.setMyLocationEnabled(true);
     }
-
     /**
      * Kích hoạt lớp Vị trí của tôi nếu quyền truy cập vị trí chính xác đã được cấp.
      */
@@ -148,6 +218,6 @@ public class HomeActivity extends AppCompatActivity implements
             permissionDenied = false;
         }
     }
-
+    //end location
 
 }
---
+++ app/src/main/java/com/example/gamepuzzlepursuit/QuestionAnswer.java
@@ -0,0 +1,17 @@
+package com.example.gamepuzzlepursuit;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Ngôn ngữ nào không phải là ngôn ngữ lập trình?",
+    };
+
+    public static String choices[][] = {
+            {"Java","Kotlin","Notepad","Python"},
+    };
+
+    public static String correctAnswers[] = {
+            "Notepad",
+    };
+
+}
